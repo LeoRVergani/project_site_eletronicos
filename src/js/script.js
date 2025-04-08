2a5f16c80f38aba60cb57fe373c8a6a99243c035
leorvergani <vergani@outlook.com.br>
2025-04-08 11:12:05 -0300

    :hammer: feat: fix banner

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,12 @@
 function trocarImagem() {
     const imagem = document.getElementById("banner");
 
-    if (window.innerWidth < 640) {
-        imagem.src = "./src/img/banner-mobile.png";
-    } else if (window.innerWidth < 768) {
-        imagem.src = "./src/img/banner600.png";
-    } else if (window.innerWidth < 1024) {
-            imagem.src = "./src/img/banner-tablet-800.png";
-    } else if (window.innerWidth < 1535){
-        imagem.src = "./src/img/banner-desktop-1024.png";
-    }else {
-        imagem.src = "./src/img/banner-desktop.png";
-    }
+    if      (window.innerWidth < 375) { imagem.src = "./src/img/banner320.png";} 
+    else if (window.innerWidth < 640) { imagem.src = "./src/img/banner320.png";} 
+    else if (window.innerWidth < 768) { imagem.src = "./src/img/banner600.png";} 
+    else if (window.innerWidth < 1024) { imagem.src = "./src/img/banner-tablet-800.png";} 
+    else if (window.innerWidth < 1535){ imagem.src = "./src/img/banner-desktop-1024.png";}
+    else { imagem.src = "./src/img/banner-desktop.png";}
 }
     window.addEventListener("load", trocarImagem);
     window.addEventListener("resize", trocarImagem);
